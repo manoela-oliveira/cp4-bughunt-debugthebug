f980cb0c467a6dd3dba4c0ed4020f6fa28203904
manoela-oliveira <manoela.oliveira0900@gmail.com>
2026-09-10 20:57:28 -0300

    fix: bug02 - adicionando geracao de id automatico

--- streamfiap-bughunt/src/main/java/br/com/fiap/streamfiap/model/Usuario.java
+++ streamfiap-bughunt/src/main/java/br/com/fiap/streamfiap/model/Usuario.java
@@ -3,6 +3,8 @@ package br.com.fiap.streamfiap.model;
 import br.com.fiap.streamfiap.exception.ClassificacaoIndicativaException;
 import br.com.fiap.streamfiap.exception.CreditosInsuficientesException;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
@@ -11,6 +13,7 @@ import jakarta.persistence.Table;
 public class Usuario {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // Correção: garantindo geração automática
     private Long id;
 
     private String nome;
