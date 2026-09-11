fd50f6fa5b4b1e3ba2e14d394837fd95024748ee
manoela-oliveira <manoela.oliveira0900@gmail.com>
2026-09-10 21:14:18 -0300

    fix: bug03 - removendo try-catch vazio

--- streamfiap-bughunt/src/main/java/br/com/fiap/streamfiap/controller/ConteudoController.java
+++ streamfiap-bughunt/src/main/java/br/com/fiap/streamfiap/controller/ConteudoController.java
@@ -1,17 +1,23 @@
 package br.com.fiap.streamfiap.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 import br.com.fiap.streamfiap.exception.ConteudoNaoEncontradoException;
 import br.com.fiap.streamfiap.model.Conteudo;
 import br.com.fiap.streamfiap.model.Documentario;
 import br.com.fiap.streamfiap.model.Filme;
 import br.com.fiap.streamfiap.model.Serie;
 import br.com.fiap.streamfiap.repository.ConteudoRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.ArrayList;
-import java.util.List;
 
 @RestController
 @RequestMapping("/api/conteudos")
@@ -29,14 +35,9 @@ public class ConteudoController {
     // GET /api/conteudos/{id} - Buscar por ID
     @GetMapping("/{id}")
     public Conteudo buscarPorId(@PathVariable Long id) {
-        try {
             Conteudo conteudo = conteudoRepository.findById(id)
                     .orElseThrow(() -> new ConteudoNaoEncontradoException("Conteúdo não encontrado: " + id));
             return ResponseEntity.ok(conteudo).getBody();
-        } catch (Exception e) {
-            // TODO: tratar isso depois
-        }
-        return null;
     }
 
     // GET /api/conteudos/categoria/{categoria} - Buscar por categoria
