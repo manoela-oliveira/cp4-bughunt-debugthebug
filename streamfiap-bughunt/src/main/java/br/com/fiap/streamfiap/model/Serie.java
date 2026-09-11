c79fe63a4ca6d9ffba9c0ed4cf3bc5471329373d
manoela-oliveira <manoela.oliveira0900@gmail.com>
2026-09-10 22:03:53 -0300

    fix: bug07 - corrigindo assinatura para utilizar o Override

--- streamfiap-bughunt/src/main/java/br/com/fiap/streamfiap/model/Serie.java
+++ streamfiap-bughunt/src/main/java/br/com/fiap/streamfiap/model/Serie.java
@@ -12,13 +12,15 @@ public class Serie extends Conteudo implements Promocionavel {
 
     // cria a série com os dados recebidos
     public Serie(String titulo, String categoria, int duracaoMinutos, int classificacaoEtaria, int numeroTemporadas) {
-        // Correcão: adicionando super para garantir que os dados sejam repassados para a Classe Mãe
+        // Correcão: adicionando super para garantir que os dados sejam repassados para a classe mãe
         super(titulo, categoria, duracaoMinutos, classificacaoEtaria, true);
         this.numeroTemporadas = numeroTemporadas;
     }
 
     // preço da série: 4.90 por temporada
-    public double calcularPrecoAluguel(double desconto) {
+    // Correção: adicionando Override para garantir a substituição e removendo o parâmetro inexistente na classe mãe (double desconto)
+    @Override 
+    public double calcularPrecoAluguel() { 
         return 4.90 * numeroTemporadas;
     }
 
